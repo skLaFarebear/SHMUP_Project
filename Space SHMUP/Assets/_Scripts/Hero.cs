ea6e7c21c3ff8fadf17882316f6ff659cb78196b
Farris Awadallah <awadallah.farris@gmail.com>
2015-02-26 19:38:03 -0500

    First file to commit to Github
    Almost finished project

---
+++ Space SHMUP/Assets/_Scripts/Hero.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hero : MonoBehaviour {
+	static public Hero S; //Singleton
+
+	public float gameRestartDelay = 2f;
+
+	//Control movement of ship
+	public float speed =30;
+	public float rollMult = -45;
+	public float pitchMult = 30;
+
+	//ship status information
+	[SerializeField]
+	private float _shieldLevel = 1;
+
+	public Weapon[] weapons;
+
+	public bool ______________________;
+
+	public Bounds bounds;
+
+	public delegate void WeaponFireDelegate();
+	public WeaponFireDelegate fireDelegate;
+
+	void Awake(){
+		S = this; // set singleton
+		bounds = Utils.CombineBoundsOfChildren (this.gameObject);
+	}
+
+	void Start(){
+		ClearWeapons ();
+		weapons [0].SetType (WeaponType.blaster);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//pull in information from the input
+		float xAxis = Input.GetAxis ("Horizontal");
+		float yAxis = Input.GetAxis ("Vertical");
+		Vector3 pos = transform.position;
+		pos.x += xAxis * speed * Time.deltaTime;
+		pos.y += yAxis * speed * Time.deltaTime;
+		transform.position = pos;
+
+		bounds.center = transform.position;
+		Vector3 off = Utils.ScreenBoundsCheck (bounds, BoundsTest.onScreen);
+		if(off!= Vector3.zero){
+			pos -= off;
+			transform.position = pos;
+		}
+		//rotate the ship to make feel more dynamic
+		transform.rotation = Quaternion.Euler (yAxis * pitchMult, xAxis * rollMult, 0);
+	}
+
+	public GameObject lastTriggerGo = null;
+
+
+	void OnTriggerEnter(Collider other){
+		GameObject go = Utils.FindTaggedParent (other.gameObject);
+		if(go!= null){
+			if(go == lastTriggerGo){
+				return;
+			}
+			lastTriggerGo = go;
+			if(go.tag == "Enemy"){
+				shieldLevel--;
+				Destroy(go);
+			}else if(go.tag == "PowerUp"){
+				AbsorbPowerUp(go);
+			}else{
+				print("Triggered: " + go.name);
+			}
+		}else{
+			print ("Triggered: " + other.gameObject.name);
+		}
+	}
+
+	public float shieldLevel{
+		get{
+			return(_shieldLevel);
+		}
+		set{
+			_shieldLevel = Mathf.Min(value, 4);
+			if(value<0){
+				Destroy(this.gameObject);
+				Main.S.DelayedRestart(gameRestartDelay);
+			}
+		}
+	}
+
+	public void AbsorbPowerUp(GameObject go){
+		PowerUp pu = go.GetComponent<PowerUp>();
+		switch (pu.type){
+		case WeaponType.shield:
+			shieldLevel++;
+			break;
+		default:
+			if(pu.type == weapons[0].type){
+				Weapon w = GetEmptyWeaponSlot();
+				if(w != null){
+					w.SetType(pu.type);
+				}
+			}else{
+				ClearWeapons();
+				weapons[0].SetType(pu.type);
+			}
+			break;
+		}
+		pu.AbsorbedBy(this.gameObject);
+	}
+	
+	Weapon GetEmptyWeaponSlot(){
+		for(int i = 0; i <weapons.Length; i++){
+			if(weapons[i].type == WeaponType.none){
+				return(weapons[i]);
+			}
+		}
+		return (null);
+	}
+	
+	void ClearWeapons(){
+		foreach(Weapon w in weapons){
+			w.SetType(WeaponType.none);
+		}
+	}
+
+
+}
---
+++ Space SHMUP/Assets/_Scripts/Shield.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+	public float rotationsPerSecond = 0.1f;
+	public bool ______________________;
+	public int levelShown = 0;
+
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = Hero.S.transform.position;
+		//read the current shiel level from the hero singleton
+		int currLevel = Mathf.FloorToInt (Hero.S.shieldLevel);
+		//if this is different from levelShown
+		if(levelShown!=currLevel){
+			levelShown = currLevel;
+			Material mat = this.renderer.material;
+			//adjust the texture offset to show different shield level
+			mat.mainTextureOffset = new Vector2(0.2f*levelShown, 0);
+		}
+		//rotate the shield a big every second
+		float rZ = (rotationsPerSecond * Time.time * 360) % 360f;
+		transform.rotation = Quaternion.Euler (0, 0, rZ);
+	}
+}
